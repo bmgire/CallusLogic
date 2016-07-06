b58f2ebde1367ab176ca1e5dc8cc3a3a625de445
Ben Gire <bmgire@gmail.com>
2016-07-06 13:16:06 -0700

    Initial Commit

---
+++ CallusLogic/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CallusLogic
+//
+//  Created by Ben Gire on 7/6/16.
+//  Copyright © 2016 Gire. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
