b18d3f98421b412077745ebd744d15fae3498eb0
Ben Gire <bmgire@gmail.com>
2017-12-02 10:32:27 -0800

    nothing major done, just committing before uploading to previous undo-fret number bug fix

---
+++ CallusLogic/NoteArraysCreator.swift
@@ -0,0 +1,27 @@
+//
+//  NoteArraysCreator.swift
+//  Callus Logic
+//
+//  Created by Ben Gire on 12/1/17.
+//  Copyright © 2017 Gire. All rights reserved.
+//
+
+import Cocoa
+
+class NoteArraysCreator {
+    
+    fileprivate var zeroTo46Array = [String]
+    fileprivate var zeroTo11Array = [String]
+    fileprivate var intervalsArray = [String]
+    fileprivate var NotesArray = [String]
+    
+
+    
+    
+    
+    fileprivate func createZeroTo46Array() {
+        
+    }
+    
+}
+
